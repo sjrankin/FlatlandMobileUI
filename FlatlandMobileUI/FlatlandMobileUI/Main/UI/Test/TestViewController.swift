1d09d25488e76327e174f5def58baddb186fef62
Stuart Rankin <sjrankin@gmail.com>
2021-08-07 14:12:31 -0700

    Create TestViewController.swift
    Code to run main test view UI.

---
+++ FlatlandMobileUI/FlatlandMobileUI/Main/UI/Test/TestViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TestViewController.swift
+//  TestViewController
+//
+//  Created by Stuart Rankin on 8/7/21.
+//
+
+import Foundation
+import UIKit
+
+class TestViewController: UITableViewController
+{
+    @IBAction func DoneButtonHandler(_ sender: Any)
+    {
+        dismiss(animated: true, completion: nil)
+    }
+}
