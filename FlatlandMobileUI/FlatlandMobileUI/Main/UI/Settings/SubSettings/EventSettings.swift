e769d09857c84795480f0bdd18a5459008dd800c
Stuart Rankin <sjrankin@gmail.com>
2021-08-07 14:11:06 -0700

    Create EventSettings.swift
    Code to run event settings UI.

---
+++ FlatlandMobileUI/FlatlandMobileUI/Main/UI/Settings/SubSettings/EventSettings.swift
@@ -0,0 +1,17 @@
+//
+//  EventSettings.swift
+//  EventSettings
+//
+//  Created by Stuart Rankin on 8/6/21.
+//
+
+import Foundation
+import UIKit
+
+class EventSettings: UITableViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+}
