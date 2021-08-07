1df1b8dea11f25f68423cfd823b908036ceaef8d
Stuart Rankin <sjrankin@gmail.com>
2021-08-07 14:12:00 -0700

    Create SoundEventEditorProtocol.swift
    Protocol for setting event data and sounds.

---
+++ FlatlandMobileUI/FlatlandMobileUI/Main/UI/Settings/SubSettings/SoundEventEditorProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  SoundEventEditorProtocol.swift
+//  SoundEventEditorProtocol
+//
+//  Created by Stuart Rankin on 8/6/21.
+//
+
+import Foundation
+import UIKit
+
+protocol SoundEventEditorProtocol: AnyObject
+{
+    func SetEventData(Name: String, Sound: String)
+}
